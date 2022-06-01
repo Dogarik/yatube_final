5efa66ad06931cd9452441e1b080196ed3e5367a
Rustam Magomedov <dogarik2007@yandex.ru>
2022-06-01 23:42:50 +0800

    Спинт 6. Отправка на второе ревью. Внес небольшие исправление

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
